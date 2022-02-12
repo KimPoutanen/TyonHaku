62f6c60cb6bf9a0117ffb1ecb237db4c1c7d6607
sheud <Kim.Poutanen@student.lab.fi>
2022-02-12 18:40:41 +0200

    välivaihe, scriptin alotus ja buttonit tehny

---
+++ js/peliScript.js
@@ -0,0 +1,12 @@
+
+var form = document.getElementById("form");
+var auto1nopeus = document.getElementById("auto1").value;
+var auto2nopeus = document.getElementById("auto2").value;
+var matka = document.getElementById("Matka").value;
+autoVaihtoehto = document.getElementById("autoValinta").value;
+
+form.addEventListener("submit", function kps(event) {
+	event.preventDefault() 
+   
+
+});
