0d27f3939aa8b42452344eb65dd614fd465cbd1b
sheud <Kim.Poutanen@student.lab.fi>
2022-02-12 20:50:31 +0200

    kps korjattu

--- js/peliScript.js
+++ js/peliScript.js
@@ -4,9 +4,9 @@ var tasapeli = 0
 var rounds = 0 
 var tietokone = 0;
 
-var btn3 = document.getElementsByClassName("kiviButton");
+var btn3 = document.getElementById("kiviButton");
 var btn1 = document.getElementById("paperiButton");
-var btn2 = document.getElementByClass("saksetButton");
+var btn2 = document.getElementById("saksetButton");
 
 
 function tietokoneValinta(){
@@ -28,9 +28,9 @@ function tietokoneValinta(){
 btn1.addEventListener("click", function kpsPaperi(event) {
 	event.preventDefault() 
    var tiettari = tietokoneValinta();
-   if (tiettari == "Paperi") {
+   if (tiettari === "Paperi") {
        return document.getElementById("vastaus").innerHTML = "Tietokone valitsi myös paperin. Tasapeli!"
-   } else if(tiettari == "Sakset") {
+   } else if(tiettari === "Sakset") {
        return document.getElementById("vastaus").innerHTML = "Tietokone valitsi sakset. Hävisit pelin!"
    } else {
     return document.getElementById("vastaus").innerHTML = "Tietokone valitsi kiven. Voitit pelin!"
@@ -42,9 +42,9 @@ btn2.addEventListener("click", function kpsSakset(event) {
 	event.preventDefault() 
     
     var tiettari = tietokoneValinta();
-   if (tiettari == "Paperi") {
+   if (tiettari === "Paperi") {
        return document.getElementById("vastaus").innerHTML = "Tietokone valitsi paperin. Voitit pelin!"
-   } else if(tiettari == "Sakset") {
+   } else if(tiettari === "Sakset") {
     return document.getElementById("vastaus").innerHTML = "Tietokone valitsi sakset. Tasapeli!"
    } else {
     return document.getElementById("vastaus").innerHTML = "Tietokone valitsi kiven. Hävisit pelin!"
@@ -56,9 +56,9 @@ btn3.addEventListener("click", function kpsKivi(event) {
 	event.preventDefault() 
     
     var tiettari = tietokoneValinta();
-   if (tiettari == "Paperi") {
+   if (tiettari === "Paperi") {
        return document.getElementById("vastaus").innerHTML = "Tietokone valitsi paperin. Hävisit pelin!"
-   } else if(tiettari == "Sakset") {
+   } else if(tiettari === "Sakset") {
          return document.getElementById("vastaus").innerHTML = "Tietokone valitsi sakset. Voitit pelin!"
    } else {
     return document.getElementById("vastaus").innerHTML = "Tietokone valitsi kiven. Voitit pelin!"
